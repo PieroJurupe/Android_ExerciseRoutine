f417777e6579ce65e8cae92dc74485750d7fc007
Piero Jurupe <pierojurupeq@gmail.com>
2024-08-17 15:32:43 -0500

    Agregado splash

--- app/src/main/java/com/example/gymapp/SecondActivity.java
+++ app/src/main/java/com/example/gymapp/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.gymapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,8 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Imagebuttonclicked(View view) {
     }
 }
